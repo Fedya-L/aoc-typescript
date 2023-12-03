5b4883e9d275a146e66c527ee50fd7898974fbdc
Fedya Levkin <f.levkin@gmail.com>
2023-12-03 19:10:18 +0200

    y2023d03t1 solved

--- 2023/03.ts
+++ 2023/03.ts
@@ -18,25 +18,38 @@ class ConvenientArray2D<T> {
 
     get(x: number, y: number): T | undefined {
         if (
-            this.data.length <= x ||
-            this.data[x].length <= y
+            y < 0 ||
+            x < 0 ||
+            this.data.length <= y ||
+            this.data[y].length <= x
         ) {
             return undefined
         }
-        return this.data[x][y]
+        return this.data[y][x]
     }
 
 }
 
-function parseMapToSymbolsMap(input: string): ConvenientArray2D<boolean> {
-    const lines = input.split("\n")
+type Coordinate2d = {
+    x: number
+    y: number
+}
+
+type SchemeNumber = {
+    value: number
+    coordinates: Coordinate2d[]
+    boundaryCoordinates: Coordinate2d[]
+}
+
+function parseRawSchemeToSymbolsMap(rawScheme: string): ConvenientArray2D<boolean> {
+    const lines = rawScheme.split("\n")
     const rowsOfCharacters = lines.map(l => l.split(''))
 
     let symbolsMap: boolean[][] = []
     for (const [y, chars] of rowsOfCharacters.entries()) {
         let xa: boolean[] = []
         for (const [x, char] of chars.entries()) {
-            xa[x] = char !== '.'
+            xa[x] = (char !== '.' && !isNumber(char))
         }
         symbolsMap[y] = xa
     }
@@ -45,10 +58,92 @@ function parseMapToSymbolsMap(input: string): ConvenientArray2D<boolean> {
 }
 
 
+function getSurroundingCoordinates2DForCoordinates2D(inputCoordinates: Coordinate2d[]): Coordinate2d[] {
+    let outCoordinates: Coordinate2d[] = []
+    let coordinatesMappingArray = [
+        [-1, -1], [0, -1], [1, -1],
+        [-1, 0],           [1, 0],
+        [-1, 1],  [0, 1],  [1, 1],
+    ]
+    for (const coordinate of inputCoordinates) {
+        for (const [x, y] of coordinatesMappingArray) {
+            const sc = {
+                x: coordinate.x + x,
+                y: coordinate.y + y,
+            }
+            if (
+                outCoordinates.some((c) => c.x == sc.x && c.y == sc.y) ||
+                inputCoordinates.some((c) => c.x == sc.x && c.y == sc.y) 
+            ) { 
+                continue
+            }
+            outCoordinates.push(sc)
+        }
+    }
+
+
+    return outCoordinates
+}
+
+function isNumber(possibleNumberOrNot: string): boolean {
+    return !isNaN(+possibleNumberOrNot)
+}
+
+function parseRawSchemeToSchemeNumbers(rawScheme: string): SchemeNumber[] {
+    const lines = rawScheme.split("\n")
+    const rowsOfCharacters = lines.map(l => l.split(''))
+
+
+    let schemeNumbers: SchemeNumber[] = []
+    for (const [y, chars] of rowsOfCharacters.entries()) {
+        let digits: string[] = []
+        let coordinates: Coordinate2d[] = []
+        for (const [x, char] of chars.entries()) {
+            if (isNumber(char)) {
+                digits.push(char)
+                coordinates.push({x, y})
+            } else if (digits.length > 0) {
+                schemeNumbers.push({
+                    value: +digits.join(''),
+                    coordinates,
+                    boundaryCoordinates: getSurroundingCoordinates2DForCoordinates2D(coordinates),
+                })
+                digits = []
+                coordinates = []
+            }
+        }
+        if (digits.length > 0) {
+            schemeNumbers.push({
+                value: +digits.join(''),
+                coordinates,
+                boundaryCoordinates: getSurroundingCoordinates2DForCoordinates2D(coordinates),
+            })
+        }
+    } 
+
+    return schemeNumbers
+}
+
+function findWaldoErrPartNumbers(schemeNumbers: SchemeNumber[], symbolsMap: ConvenientArray2D<boolean>): SchemeNumber[] {
+
+    return schemeNumbers.filter((schemeNumbers) => {
+        for (const boundaryCoordinates of schemeNumbers.boundaryCoordinates) {
+            const isSymbol = symbolsMap.get(boundaryCoordinates.x, boundaryCoordinates.y)
+            if (isSymbol) {
+                return true
+            }
+        }
+        return false
+    })
+    
+}
+
+
 export function solve1(input: string): number {
-    const symbolsMap = parseMapToSymbolsMap(input)
-    const res = symbolsMap.get(0,0)
-    return -1
+    const symbolsMap = parseRawSchemeToSymbolsMap(input)
+    const schemeNumbers = parseRawSchemeToSchemeNumbers(input)
+    const partNumbers = findWaldoErrPartNumbers(schemeNumbers, symbolsMap)
+    return partNumbers.reduce((p, c) => p + c.value, 0)
 }
 
 export function solve2(): number {
