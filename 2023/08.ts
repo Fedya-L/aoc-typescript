a518c244b64e8ce5e3bf73bf9fb37de0f0b22930
Fedya Levkin <f.levkin@gmail.com>
2023-12-09 00:49:52 +0200

    Checkpoint

--- 2023/08.ts
+++ 2023/08.ts
@@ -1,3 +1,4 @@
+import { Z_STREAM_END } from "zlib";
 
 function inputToStepsAndAndJunctions(input: string): [number[], { [key: string]: string[] }] {
 
@@ -37,24 +38,109 @@ export function solve1(input: string): number {
     return -1
 }
 
+type State = {
+    stepCount: number
+    stepIndex: number
+    junction: string
+}
+
+type StateWithDiff = State & {
+    diffToPrev: number
+}
+
 export function solve2(input: string): number {
 
-    const [steps, junction] = inputToStepsAndAndJunctions(input)
+    const [steps, junctions] = inputToStepsAndAndJunctions(input)
 
-    let currentJunctions = Object.keys(junction).filter(j => j.endsWith('A'))
-    let stepCount = 0
-    while (true) { 
-        const nextStep = steps[stepCount % steps.length]
-        stepCount++
+    const startingJunctions = Object.keys(junctions).filter(j => j.endsWith('A'))
 
-        for (let i = 0; i < currentJunctions.length; i++) {
-            currentJunctions[i] = junction[currentJunctions[i]][nextStep]            
+    let zStateByStartingJunction: { [key: string]: StateWithDiff[] } = {}
+
+    for (const startingJunction of startingJunctions) {
+        let currentJunction = startingJunction
+        let zEntries: State[] = []
+
+
+        let stepCount = 0
+        while (zEntries.length < 10) {
+            let stepIndex = stepCount % steps.length
+            let stepInstruction = steps[stepIndex]
+            stepCount++
+    
+            currentJunction = junctions[currentJunction][stepInstruction]
+            
+            if (currentJunction.endsWith('A')) {
+                console.log('breakpoint')
+            }
+            if (currentJunction.endsWith('Z')) {
+                zEntries.push({
+                    stepIndex,
+                    stepCount,
+                    junction: currentJunction,
+                })
+            }
+
         }
 
-        if (currentJunctions.length === currentJunctions.filter(j => j.endsWith('Z')).length) {
-            return stepCount
+        let zEntriesWithDiff: StateWithDiff[] = [
+            {...zEntries[0], diffToPrev: zEntries[0].stepCount}
+        ]
+        for (let i = 1; i < zEntries.length; i++) {
+            const prev = zEntries[i-1]
+            const curr = zEntries[i]
+
+            zEntriesWithDiff.push({
+                ...curr,
+                diffToPrev: curr.stepCount - prev.stepCount
+            })
         }
+
+        zStateByStartingJunction[startingJunction] = zEntriesWithDiff
+
     }
 
+    const startOfCycle = Object.entries(zStateByStartingJunction).map(([sj, ze]) => {
+        return {
+            junction: sj,
+            lastPreCycle: ze[0].diffToPrev - ze[1].diffToPrev,
+            start: ze[0].diffToPrev - ze[1].diffToPrev + 1,
+            end: ze[0],
+            length: ze[1].diffToPrev,
+            div: (ze[0].diffToPrev - ze[1].diffToPrev) / 13019,
+
+        }
+    })
+
+    // let numToCount: {[key: number]: number} = {}
+    let numToCount: number[] = []
+
+    let junctionsCount = startingJunctions.length
+    let countCompare = startingJunctions.length - 1
+    for (let i = 1; i < 1_000_000_000; i++) {
+        
+        // for (onst cycle of startOfCycle) {
+            const newEnd = cycle.lastPreCycle + cycle.length * i
+
+            let checkPassed = startOfCycle.filter(j => {
+                return newEnd % j.length == j.lastPreCycle 
+            }).length
+            if (checkPassed != junctionsCount) {
+                continue
+            }
+            let count = numToCount[newEnd]
+            if (count) {
+                if (count == countCompare) {
+                    return newEnd
+                }
+                numToCount[newEnd] = count + 1
+            } else {
+                numToCount[newEnd] = 1
+            }
+        // }
+
+
+    }
+
+
     return -2
-}
+}   
