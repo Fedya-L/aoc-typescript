df45fc39a7bf69d7448b5c1f1119796a6e40b231
Fedya Levkin <f.levkin@gmail.com>
2023-12-20 02:50:27 +0200

    y2023d19

--- 2023/19.ts
+++ 2023/19.ts
@@ -129,6 +129,173 @@ export const solve1 = (input: string): number => {
     return result
 }
 
+type Range = {
+    min: number
+    // Inclusive
+    max: number
+}
+
+
+type PartWithRanges = {
+    x: Range
+    m: Range
+    a: Range
+    s: Range
+}
+
+type State = {
+    workflowId: string
+    ruleIndex: number
+    part: PartWithRanges
+}
+
+const splitRangeFrom = (range: Range, splitValue: number): [Range, Range] => {
+    let smallerRange = {...range}
+    let largerRange = {...range}
+
+    smallerRange.max = splitValue - 1
+    largerRange.min = splitValue
+
+    return [smallerRange, largerRange]
+}
+
 export const solve2 = (input: string): number => {
-    return -2
+
+    const [workflows] = inputToWorkflowsAndParts(input)
+
+    let statesToProcess: State[] = [
+        {
+            workflowId: 'in',
+            ruleIndex: 0,
+            part: {
+                x: {min: 1, max: 4000},
+                m: {min: 1, max: 4000},
+                a: {min: 1, max: 4000},
+                s: {min: 1, max: 4000},
+            }
+        }
+    ]
+
+    let acceptedParts: PartWithRanges[] = []
+
+    while (statesToProcess.length) {
+        const s = statesToProcess.pop()!
+        const p = s.part
+        const w = workflows.get(s.workflowId)!
+
+        if (s.workflowId === 'A') {
+            acceptedParts.push(s.part)
+            continue
+        }
+        if (s.workflowId === 'R') {
+            continue
+        }
+
+        const r = w.rules[s.ruleIndex]
+        if (r.value === 0) {
+            statesToProcess.push({
+                workflowId: r.target,
+                ruleIndex: 0,
+                part: s.part
+            })
+            continue
+        }
+
+        const testedRange = p[r.category] as Range
+        if (r.shouldBeBigger) {
+            if (testedRange.min > r.value) {
+                // Pass the full range to next workflow
+                statesToProcess.push({
+                    workflowId: r.target,
+                    ruleIndex: 0,
+                    part: p
+                })
+            } else if (testedRange.max <= r.value) {
+                // Pass the full range to next rule
+                statesToProcess.push({
+                    workflowId: s.workflowId,
+                    ruleIndex: s.ruleIndex + 1,
+                    part: p
+                })
+            } else {
+                // Now we will split
+                const [lowerRange, higherRange] = splitRangeFrom(testedRange, r.value + 1)
+
+                // Higher range will go to next workflow
+                let higherPart = {...p}
+                higherPart[r.category] = higherRange
+                statesToProcess.push({
+                    workflowId: r.target,
+                    ruleIndex: 0,
+                    part: higherPart
+                })
+                // Lower range will go to next rule
+                let lowerPart = {...p}
+                lowerPart[r.category] = lowerRange
+                statesToProcess.push({
+                    workflowId: s.workflowId,
+                    ruleIndex: s.ruleIndex + 1,
+                    part: lowerPart
+                })
+            }
+        } else {
+            if (testedRange.max < r.value) {
+                // Pass the full range to next workflow
+                statesToProcess.push({
+                    workflowId: r.target,
+                    ruleIndex: 0,
+                    part: p
+                })
+            } else if (testedRange.min >= r.value) {
+                // Pass the full range to next rule
+                statesToProcess.push({
+                    workflowId: s.workflowId,
+                    ruleIndex: s.ruleIndex + 1,
+                    part: p
+                })
+            } else {
+                // Now we will split
+                const [lowerRange, higherRange] = splitRangeFrom(testedRange, r.value)
+
+                // Higher range will go to next rule
+                let higherPart = {...p}
+                higherPart[r.category] = higherRange
+                statesToProcess.push({
+                    workflowId: s.workflowId,
+                    ruleIndex: s.ruleIndex + 1,
+                    part: higherPart
+                })
+                // Lower range will go to next workflow
+                let lowerPart = {...p}
+                lowerPart[r.category] = lowerRange
+                statesToProcess.push({
+                    workflowId: r.target,
+                    ruleIndex: 0,
+                    part: lowerPart
+                })
+            }
+        }
+    } 
+
+    const result = acceptedParts.map((p) => {
+        p.x.max -= p.x.min - 1
+        p.x.min -= p.x.min - 1
+
+        p.m.max -= p.m.min - 1
+        p.m.min -= p.m.min - 1
+
+        p.a.max -= p.a.min - 1
+        p.a.min -= p.a.min - 1
+
+        p.s.max -= p.s.min - 1
+        p.s.min -= p.s.min - 1
+        return (
+            p.x.max * 
+            p.m.max * 
+            p.a.max * 
+            p.s.max
+        )
+    }).reduce((p,c) => p + c, 0)
+
+    return result
 }
