69664ca3e37eef4ff3babd144b43727422f8b781
Fedya Levkin <f.levkin@gmail.com>
2023-12-24 01:34:56 +0200

    y2023d23p1

--- 2023/23.ts
+++ 2023/23.ts
@@ -1,6 +1,123 @@
+import { ConvenientArray2D, Coordinate2D } from "../utils/data-things"
+
+type Tile = '#' | '.' | '>' | '<' | 'v' | '^'
+type HikingMap = ConvenientArray2D<Tile>
+
+
+
+
+function inputToHikingMap(i: string): HikingMap {
+    return new ConvenientArray2D<Tile>(i.split("\n").map(l => l.split('') as Tile[]))
+}
+
+class CoordinateSet {
+    data: Set<string>
+
+    constructor(data: Set<string> | undefined = undefined) {
+        this.data = data ?? new Set()
+    }
+
+    add(c: Coordinate2D) {
+        this.data.add(this.coordinateToKey(c))
+    }
+
+    has(c: Coordinate2D) {
+        return this.data.has(this.coordinateToKey(c))
+    }
+
+    coordinateToKey(c: Coordinate2D): string {
+        return `${c.x},${c.y}`
+    }
+
+}
+
+const possibleCoordinates: {[key: string]: [number, number][]} = {
+    '#': [],
+    '.': [
+        [-1,0],
+        [1, 0],
+        [0,-1],
+        [0,1],
+    ],
+    '>': [[1,0]],
+    '<': [[-1,0]],
+    'v': [[0,1]],
+    '^': [[0,-1]],
+}
+
+function getNextCoordinates(c: Coordinate2D, hikingMap: HikingMap, visitedCoordinates: CoordinateSet): Coordinate2D[] {
+    const currentTile = hikingMap.getXY(c)!
+    const ps = possibleCoordinates[currentTile].map(([x,y]) => ({x: c.x + x, y: c.y + y})).filter(c => !visitedCoordinates.has(c))
+    if (ps.length === 0) {
+        return []
+    }
+
+    let nextCoordinates: Coordinate2D[] = []
+    for (const cc of ps) {
+        const tile = hikingMap.getXY(cc) ?? '#'
+        if (tile === '#') {
+            continue
+        }
+        if (tile === '.') {
+            nextCoordinates.push(cc)
+            continue
+        }
+        if (tile === '>') {
+            if (cc.x - c.x !== -1) {
+                nextCoordinates.push(cc)
+            }
+            continue
+        }
+        if (tile === '<') {
+            if (cc.x - c.x !== 1) {
+                nextCoordinates.push(cc)
+            }
+            continue
+        }
+        if (tile === 'v') {
+            if (cc.y - c.y !== -1) {
+                nextCoordinates.push(cc)
+            }
+            continue
+        }
+        if (tile === '^') {
+            if (cc.y - c.y !== 1) {
+                nextCoordinates.push(cc)
+            }
+            continue
+        }
+    }
+
+    return nextCoordinates
+}
+
+// Returns possible paths (lenghts)
+function traversePath(currentPoint: Coordinate2D, finishPoint: Coordinate2D, hikingMap: HikingMap, visitedCoordinates: CoordinateSet): number[] {
+    if (currentPoint.x === finishPoint.x && currentPoint.y === finishPoint.y) {
+        return [visitedCoordinates.data.size]
+    }
+    visitedCoordinates.add(currentPoint)
+    const nextCoordinates = getNextCoordinates(currentPoint, hikingMap, visitedCoordinates)
+    if (nextCoordinates.length === 0) {
+        return []
+    }
+    if (nextCoordinates.length === 1) {
+        return traversePath(nextCoordinates[0], finishPoint, hikingMap, visitedCoordinates)
+    }
+
+    return nextCoordinates.flatMap(c => traversePath(c, finishPoint, hikingMap, new CoordinateSet(new Set([...visitedCoordinates.data]))))
+}
 
 export function solve1(i: string): number {
-    return -1
+
+    const hikingMap = inputToHikingMap(i)
+    const startingPoint: Coordinate2D = {x: 1, y: 0}
+    const finishPoint: Coordinate2D = {x: hikingMap.xSize - 2, y: hikingMap.ySize - 1}
+
+
+    const possiblePaths = traversePath(startingPoint, finishPoint, hikingMap, new CoordinateSet())
+
+    return possiblePaths.sort((a,b) => b - a)[0]
 }
 
 export function solve2(i: string): number {
