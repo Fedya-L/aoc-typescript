cc6b3ec178e1d27ccfe1dcd0d21c48fc4b65a4fc
Fedya Levkin <f.levkin@gmail.com>
2023-12-24 10:44:08 +0200

    Checkpoint

--- 2023/23.ts
+++ 2023/23.ts
@@ -29,6 +29,9 @@ class CoordinateSet {
         return `${c.x},${c.y}`
     }
 
+    copy(): CoordinateSet {
+        return new CoordinateSet(new Set([...this.data]))
+    }
 }
 
 const possibleCoordinates: {[key: string]: [number, number][]} = {
@@ -120,6 +123,49 @@ export function solve1(i: string): number {
     return possiblePaths.sort((a,b) => b - a)[0]
 }
 
+function getNextCoordinatesV2(c: Coordinate2D, hikingMap: HikingMap, visitedCoordinates: CoordinateSet): Coordinate2D[] {
+    const currentTile = hikingMap.getXY(c)!
+    const ps = possibleCoordinates['.'].map(([x,y]) => ({x: c.x + x, y: c.y + y})).filter(c => !visitedCoordinates.has(c))
+    if (ps.length === 0) {
+        return []
+    }
+
+    let nextCoordinates: Coordinate2D[] = []
+    for (const cc of ps) {
+        const tile = hikingMap.getXY(cc) ?? '#'
+        if (tile === '#') {
+            continue
+        }
+        nextCoordinates.push(cc)
+    }
+
+    return nextCoordinates
+}
+
+// Returns possible paths (lenghts)
+function traversePathV2(currentPoint: Coordinate2D, finishPoint: Coordinate2D, hikingMap: HikingMap, visitedCoordinates: CoordinateSet): number[] {
+    if (currentPoint.x === finishPoint.x && currentPoint.y === finishPoint.y) {
+        return [visitedCoordinates.data.size]
+    }
+    visitedCoordinates.add(currentPoint)
+    const nextCoordinates = getNextCoordinatesV2(currentPoint, hikingMap, visitedCoordinates)
+    if (nextCoordinates.length === 0) {
+        return []
+    }
+    if (nextCoordinates.length === 1) {
+        return traversePathV2(nextCoordinates[0], finishPoint, hikingMap, visitedCoordinates)
+    }
+
+    return nextCoordinates.flatMap(c => traversePathV2(c, finishPoint, hikingMap, visitedCoordinates.copy()))
+}
+
 export function solve2(i: string): number {
-    return -2
+    const hikingMap = inputToHikingMap(i)
+    const startingPoint: Coordinate2D = {x: 1, y: 0}
+    const finishPoint: Coordinate2D = {x: hikingMap.xSize - 2, y: hikingMap.ySize - 1}
+
+
+    const possiblePaths = traversePathV2(startingPoint, finishPoint, hikingMap, new CoordinateSet())
+
+    return possiblePaths.sort((a,b) => b - a)[0]
 }
